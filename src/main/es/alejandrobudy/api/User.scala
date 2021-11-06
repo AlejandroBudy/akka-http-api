ab60eb60c1808e4a9d529d3a31a9cb5db529ae3b
AlejandroBudy <atorresato@gmail.com>
2021-11-06 16:08:27 +0100

    User define as aggregate root

--- src/main/es/alejandrobudy/api/User.scala
+++ src/main/es/alejandrobudy/api/User.scala
@@ -1,3 +1,21 @@
 package es.alejandrobudy.api
 
-case class User( id: String, name: String)
+import java.util.UUID
+
+case class User(id: UserId, name: UserName)
+
+object User {
+  def apply(id: String, name: String) = new User(UserId(id), UserName(name))
+}
+
+case class UserId(id: UUID)
+
+object UserId {
+  def apply(id: String) = new UserId(UUID.fromString(id))
+}
+
+case class UserName(name: String)
+
+object UserName {
+  def apply(name: String) = new UserName(name)
+}
