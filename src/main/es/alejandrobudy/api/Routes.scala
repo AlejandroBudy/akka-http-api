ba3bbf11eedd8e292d03a3f4b446508d879dde25
AlejandroBudy <atorresato@gmail.com>
2021-11-11 19:06:35 +0100

    Test using value object working

--- src/main/es/alejandrobudy/api/Routes.scala
+++ src/main/es/alejandrobudy/api/Routes.scala
@@ -2,6 +2,7 @@ package es.alejandrobudy.api
 
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 import es.alejandrobudy.api.UserMarshaller._
 
 object Routes {
@@ -14,10 +15,10 @@ object Routes {
       |""".stripMargin
 
   private val users = Seq(
-    User("1", "Alejandro"),
-    User("2", "Budy")
+    User("73aec926-7840-4a44-87e0-2d540e37e636", "Alejandro"),
+    User("73aec926-7840-4a44-87e0-2d540e37e636", "Budy")
   )
-  val all = get {
+  val all: Route = get {
     path("status") {
       complete(HttpEntity(ContentTypes.`application/json`, body))
     } ~
--- src/test/es/alejandrobudy/api/ScalaHttpApiTest.scala
+++ src/test/es/alejandrobudy/api/ScalaHttpApiTest.scala
@@ -10,12 +10,16 @@ class ScalaHttpApiTest extends WordSpec with Matchers with ScalaFutures with Sca
 
   case class UserStub(id: String, name: String)
 
+  object UserStub {
+    def apply(id: String, name: String): User = User(id, name)
+  }
+
   object UserMarshaller {
     def marshall(users: Seq[User]): JsArray = JsArray(
       users.map(user =>
         JsObject(
-          "id" -> JsString(user.id),
-          "name" -> JsString(user.name)
+          "id" -> JsString(user.id.id.toString),
+          "name" -> JsString(user.name.name)
         )
       ).toVector
     )
@@ -52,7 +56,9 @@ class ScalaHttpApiTest extends WordSpec with Matchers with ScalaFutures with Sca
       Get("/users") ~> Routes.all ~> check {
         status shouldBe StatusCodes.OK
         contentType shouldBe ContentTypes.`application/json`
-        val expectedUsers = Seq(User("1", "Alejandro"), User("2", "Budy"))
+        val expectedUsers = Seq(
+          UserStub("73aec926-7840-4a44-87e0-2d540e37e636", "Alejandro"),
+          UserStub("73aec926-7840-4a44-87e0-2d540e37e636", "Budy"))
         entityAs[String].parseJson shouldBe UserMarshaller.marshall(expectedUsers)
       }
     }
